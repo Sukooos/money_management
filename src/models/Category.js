d9aa199c7efd5c3798042413e933bcf37800fa75
Sukooos <rajazahran61@gmail.com>
2024-12-28 22:47:50 +0700

    update category

---
+++ src/models/Category.js
@@ -0,0 +1,24 @@
+// models/Category.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Category = sequelize.define('Category', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+}, {
+    tableName: 'categories',
+    timestamps: false,
+});
+
+module.exports = Category;
